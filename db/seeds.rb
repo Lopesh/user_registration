26cb99e18931d73b7db0cd37a8db8f82e896d245
Lopesh <durugkarlopesh@gmail.com>
2021-04-19 09:16:44 +0530

    Added Seed File.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!({
+                email: "adminuser@gmail.com", 
+                password: "123456", 
+                role: 1, 
+                name: "Admin", 
+                profile_attributes: {"address_line"=>"Buidling No: 11, Room No: 1221", 
+                    "street"=>"M.G.Road", 
+                    "landmark"=>"Near Worli Police Station", 
+                    "city"=>"Mumbai", 
+                    "state"=>"Maharashtra", 
+                    "pin_code"=>"400099",
+                    "phone_number": "1234567890"
+                }
+                
+            })
